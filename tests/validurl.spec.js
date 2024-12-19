ff81997ccfd92557773b276357297da9a0ae4638
Anshul <anshul.a.iper@gmail.com>
2024-12-19 19:11:31 +0530

    ifelseupdate

--- tests/validurl.spec.js
+++ tests/validurl.spec.js
@@ -5,8 +5,22 @@ test('valid URL page', async ({ page }) => {
   
   await page.goto(validUrl);
 
-  const currentUrl = page.url();
-  expect(currentUrl).toBe(validUrl);
+  const currentUrl= page.url();
+
+  if (currentUrl===validUrl){
 
   console.log('Login page URL is: ' + validUrl);
+}
+
+  else
+  {
+
+    console.log("your cuttent url is not correct" +validUrl+ ',but got'+ currentUrl)
+
+  
+
+  expect(currentUrl).tobe(validUrl)
+
+ }
+
 });
