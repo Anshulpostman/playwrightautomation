f329a0ad083b05f8f52e21cf9d0b7016db5f839d
Anshul <anshul.a.iper@gmail.com>
2024-12-20 21:45:06 +0530

    update code

--- tests/logintestcase.spec.js
+++ tests/logintestcase.spec.js
@@ -13,19 +13,20 @@ test('valid login test case' , async  ({page}) => {
 
     const dashboardpage='https://devecg.resourcifi.tech/super/dashboard';
 
-    const currentUrl='user needs to login';
+    const currentUrl="user login successfully";
 
-    if(currentUrl===dashboardpage){
+    if(dashboardpage===currentUrl){
 
-        console.log("dashboard page :", + dashboardpage )
+        console.log("dashboard page :", + currentUrl )
     }
 
     else
-    console.log("url is not corret:" +dashboardpage+ ',but got' +currentUrl)
+    console.log("url is corret:"   +currentUrl)
 
 })
 
-  //login with invaid Email id and valid password
+  
+//login with invaid Email id and valid password
   test("Invalid email and valid password" , async function({page}){
 
     await page.goto("https://devecg.resourcifi.tech/login")
@@ -91,11 +92,74 @@ test.only("login with valid email id and invalid password" , async function({pag
 
 })
 
-//
+//login with blank email and password.
+
+  test("login with blank email id and valid password" , async function({page}){
+
+    await page.goto("https://devecg.resourcifi.tech/login");
+
+    await page.locator("//input[contains(@placeholder,'Email Address')]");
+
+    await page.locator("//input[@type='password']").type("superrAdmin123");
+
+    await page.locator("//span[normalize-space()='Log In']").click();
+
+    const Emailmessage= await page.locator("//div[contains(text(),'Email is a required field.')]");
+
+    const Emailerrormessage= await Emailmessage.textContent();
+
+    if 
+       (Emailerrormessage==="Email is a required field."){
+
+       console.log("validation message is :" +Emailerrormessage )
+       }
+    
+       else{
+
+        console.log("Test case failed:" )
+
+       }
+
+       expect(Emailerrormessage).toBe("Email is a required field.")
+    
+
+  })
+
+  ////login with valid email and blank password.
+
+
+  test("login with valid email id and blank  password" , async function({page}){
 
 
 
+    await page.goto("https://devecg.resourcifi.tech/login");
 
+    await page.locator("//input[contains(@placeholder,'Email Address')]").type("superadmin@gmail.com");
+
+    await page.locator("//input[@type='password']");
+
+    await page.locator("//span[normalize-space()='Log In']").click();
+
+    const passwordmessage= await page.locator("//div[contains(text(),'Password cannot be empty.')]");
+
+    const passworderrormessage= await passwordmessage.textContent();
+
+    if 
+       (passworderrormessage==="Password cannot be empty."){
+
+       console.log("validation message is :" +passworderrormessage )
+       }
+    
+       else{
+
+        console.log("Test case failed:" )
+
+       }
+
+       expect(passworderrormessage).toBe("Password cannot be empty.");
+    
+
+    });
 
 
 
