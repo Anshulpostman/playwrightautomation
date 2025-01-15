282f89ff3e562aa77c4ea65816f0a2104e6414e1
Anshul <anshul.a.iper@gmail.com>
2025-01-15 15:39:57 +0530

    update

--- tests/inputManagement.spec.js
+++ tests/inputManagement.spec.js
@@ -168,7 +168,7 @@ test("click on input data mangement" , async({page})=>{
 
         // Test the Add input fields MBE Information
   
-        test.only("Test the add functionality of MBE" , async ({page})=>{
+        test("Test the add functionality of MBE" , async ({page})=>{
 
             const MBEsection = new Addinput(page);
             await page.setViewportSize({width : 1536 , height: 864 })
@@ -215,10 +215,14 @@ test("click on input data mangement" , async({page})=>{
 
         // test the Addinput fields in Diverse Groups Information successfully
 
-        test("test the functionality Diverse Groups Information" ,  async ({page})=>{
+        test.only("test the functionality Diverse Groups Information" ,  async ({page})=>{
         const DGIinfrmation = new Addinput(page);
         await DGIinfrmation.login();
         await DGIinfrmation.clickinputdatamangement();
+        const collapseclick= await page.locator("//div[contains(text(),'Collapse')]");
+          await page.waitForTimeout(1000);
+          await collapseclick.click();
+
 
         
 
--- tests/pratice.spec.js
+++ tests/pratice.spec.js
@@ -1,90 +1,90 @@
-const {chromium}=require('playwright');
-const { test , expect } = require('playwright/test');
-const playwrightConfig = require('./playwright.config');
+// const {chromium}=require('playwright');
+// const { test , expect } = require('playwright/test');
+// const playwrightConfig = require('./playwright.config');
 
  
-    // test("launch the browser" , async ({})=>{
+//     // test("launch the browser" , async ({})=>{
 
-    //     const browser= chromium.launch()
-    //    const page =(await browser).newPage();
-    //    await page.goto("https://www.google.com/")
-    //    console.log(await page.title());
-    //    await (await browser).close();
+//     //     const browser= chromium.launch()
+//     //    const page =(await browser).newPage();
+//     //    await page.goto("https://www.google.com/")
+//     //    console.log(await page.title());
+//     //    await (await browser).close();
     
 
-    // })
+//     // })
 
-    //take a screen Shot
+//     //take a screen Shot
 
-//     await page.screenshot({path: 'screenshot.png'})
-//     await page.screenshot({path : 'fullscreenshot.png' , fullpage: true})
+// //     await page.screenshot({path: 'screenshot.png'})
+// //     await page.screenshot({path : 'fullscreenshot.png' , fullpage: true})
 
-//     this.bowserimage="defined xpath"
-//     this.iagepath=" image of thepath.png"
+// //     this.bowserimage="defined xpath"
+// //     this.iagepath=" image of thepath.png"
 
-//     async uploadimage()
-//     if(!this.filepath) throw new error('filepath not found');
-//     await page.setinputfile(this.bowserimage , this.iagepath);
+// //     async uploadimage()
+// //     if(!this.filepath) throw new error('filepath not found');
+// //     await page.setinputfile(this.bowserimage , this.iagepath);
 
-//  PWDEBUG=1 npx playwright test for bedbugging..PWDEBUG
+// //  PWDEBUG=1 npx playwright test for bedbugging..PWDEBUG
 
-//  await page.pause() for pause the page where operation it is.
+// //  await page.pause() for pause the page where operation it is.
 
-module.exports={
-project :[
+// module.exports={
+// project :[
 
-{name: 'chromium', use :{browsername : 'chromium'}}
+// {name: 'chromium', use :{browsername : 'chromium'}}
 
-]
-}
-
-
-module.exports={
-
-    projects:[
-
-        {name: 'chromium' , use:{browsername: 'chromium'}}
-    ]
+// ]
+// }
 
 
+// module.exports={
 
-}
+//     projects:[
 
-// Launch the browser
-const {chromium} = require('playwright')
-const{test , expect} = require('@playwright/test')
- const browser= chromium.launch();
- const page= await page.browser.newpage();
- await page.goto("www.google.com");
- console.log("google chrome is launch")
+//         {name: 'chromium' , use:{browsername: 'chromium'}}
+//     ]
 
- //take the screen shot
- class screenshot{
-    constructor (page){;
-    this.page();
-    this.browseimage='xpath of browse button';
-    this.pathofimage='image of path'
-}
 
-async browseimage(){
-await this.page.click(this.browseimage , this.pathofimage)
 
-}
- }
+// }
 
- const image= new screenshot(page);
- await image.browseimage();
+// // Launch the browser
+// const {chromium} = require('playwright')
+// const{test , expect} = require('@playwright/test')
+//  const browser= chromium.launch();
+//  const page= await page.browser.newpage();
+//  await page.goto("www.google.com");
+//  console.log("google chrome is launch")
 
- //Screenshot image
- await page.screenshot({path: screenshot.png})
- //screenshot of full page
- await page.screenshot({path: 'fullscreenshot.png' , fullpage: true})
+//  //take the screen shot
+//  class screenshot{
+//     constructor (page){;
+//     this.page();
+//     this.browseimage='xpath of browse button';
+//     this.pathofimage='image of path'
+// }
 
- //inproject config : 
- module.exports={
-  Project:[
+// async browseimage(){
+// await this.page.click(this.browseimage , this.pathofimage)
+
+// }
+//  }
+
+//  const image= new screenshot(page);
+//  await image.browseimage();
+
+//  //Screenshot image
+//  await page.screenshot({path: screenshot.png})
+//  //screenshot of full page
+//  await page.screenshot({path: 'fullscreenshot.png' , fullpage: true})
+
+//  //inproject config : 
+//  module.exports={
+//   Project:[
     
-       {name: 'chromium' , use: {browsername: 'chromium'}}
+//        {name: 'chromium' , use: {browsername: 'chromium'}}
 
-  ]
-}
+//   ]
+// }
