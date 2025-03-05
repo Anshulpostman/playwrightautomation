74893b231b0ecf74ad01dec9abcb8e0d54fa90dd
Anshul <anshul.a.iper@gmail.com>
2025-03-05 15:40:00 +0530

    update

--- tests/Test.spec.js
+++ tests/Test.spec.js
@@ -1,204 +1,204 @@
-const { test, expect } = require('@playwright/test');
-const { resolveObjectURL } = require('buffer');
-const { listenerCount } = require('process');
+// const { test, expect } = require('@playwright/test');
+// const { resolveObjectURL } = require('buffer');
+// const { listenerCount } = require('process');
 
-// function isAnagram(Silent, Listen) {
-//     // Convert both strings to lowercase and sort them
-//     const str1 = Silent;
-//     const str2 = Listen
-//     return str1 === str2;
-// }
+// // function isAnagram(Silent, Listen) {
+// //     // Convert both strings to lowercase and sort them
+// //     const str1 = Silent;
+// //     const str2 = Listen
+// //     return str1 === str2;
+// // }
 
-//  test("test the anagram", async()=>{
+// //  test("test the anagram", async()=>{
 
-//     if(isAnagram("Listen", "Silent")){
+// //     if(isAnagram("Listen", "Silent")){
 
-//     expect(await isAnagram("Silent", "Listen")).toBeTruthy();
-//     console.log("It is an anagram");
-//     }
+// //     expect(await isAnagram("Silent", "Listen")).toBeTruthy();
+// //     console.log("It is an anagram");
+// //     }
 
-//     else{
-//         console.log("It is not an anagram");
-//     }
-//  })
+// //     else{
+// //         console.log("It is not an anagram");
+// //     }
+// //  })
 
-//    
-// Test the functionality of plaindrome
+// //    
+// // Test the functionality of plaindrome
 
-//   test("test the functionality of panildrome" , async({})=>{
+// //   test("test the functionality of panildrome" , async({})=>{
    
-//      const palindrome=("madam" , "hello")
-//      return str===palindrome.split().reverse().join()
+// //      const palindrome=("madam" , "hello")
+// //      return str===palindrome.split().reverse().join()
 
-//      expect(str("madam").true());
+// //      expect(str("madam").true());
 
 
-//   })
+// //   })
 
-// how do you reverse a string 
+// // how do you reverse a string 
 
-// test("test the fundtionaloty of reverse string" , async({page})=>{
+// // test("test the fundtionaloty of reverse string" , async({page})=>{
 
-//      function reversestr (str){
+// //      function reversestr (str){
 
-//      return str.split('').reverse().join('');
-//      }
-//      console.log(reversestr("Hello"))
+// //      return str.split('').reverse().join('');
+// //      }
+// //      console.log(reversestr("Hello"))
      
 
 
-// })
+// // })
 
-//Test the fundtionality without spiiting
-// test("test the fundtionaloty of reverse string without splitting" , async({page})=>{
-// function reversestr (stri){
-//  let revsaved ="";
+// //Test the fundtionality without spiiting
+// // test("test the fundtionaloty of reverse string without splitting" , async({page})=>{
+// // function reversestr (stri){
+// //  let revsaved ="";
 
-//   for(let i = stri.length-1; i >=0; i--){
+// //   for(let i = stri.length-1; i >=0; i--){
 
-//    revsaved +=stri[i]
+// //    revsaved +=stri[i]
 
     
-//    }
+// //    }
 
-//    return revsaved;
-// }
-//    console.log(reversestr("Hello"))
+// //    return revsaved;
+// // }
+// //    console.log(reversestr("Hello"))
    
 
 
-// })
+// // })
 
-// // Test the functionality in the correct order which is now in reverse order
+// // // Test the functionality in the correct order which is now in reverse order
 
-// test("Test the functionality in the correct order which is now in reverse order" , async({page})=>{
+// // test("Test the functionality in the correct order which is now in reverse order" , async({page})=>{
 
-// function correctreverse(stringcorrect){
+// // function correctreverse(stringcorrect){
 
-//    let correctrevesefixed="";
+// //    let correctrevesefixed="";
 
-//    for(let i=stringcorrect.length-1; i >=0; i--){
+// //    for(let i=stringcorrect.length-1; i >=0; i--){
 
-//       correctrevesefixed +=stringcorrect[i]
-//    }
+// //       correctrevesefixed +=stringcorrect[i]
+// //    }
 
-//       return correctrevesefixed;
+// //       return correctrevesefixed;
 
-// }
-//   console.log(correctreverse("luhsnA"))
-// })
-
-
-
-// class Anshul{
-
-//    constructor(page){
-//       this.page=page;
-//    };
+// // }
+// //   console.log(correctreverse("luhsnA"))
+// // })
 
 
-   // Mthod for reverse string
-//     correctreverse(string){
 
-//     //  return string.split('').reverse().join('');
-//       return string.split('').reverse().join('');
+// // class Anshul{
+
+// //    constructor(page){
+// //       this.page=page;
+// //    };
+
+
+//    // Mthod for reverse string
+// //     correctreverse(string){
+
+// //     //  return string.split('').reverse().join('');
+// //       return string.split('').reverse().join('');
 
       
 
      
 
-//    }
-// }
-//    test("test the functionality" , async({page})=>{
+// //    }
+// // }
+// //    test("test the functionality" , async({page})=>{
 
-//       const rev=new Anshul(page);
-//       const revresult=rev.correctreverse("luhsna");
-//       console.log("result is"+ revresult)
+// //       const rev=new Anshul(page);
+// //       const revresult=rev.correctreverse("luhsna");
+// //       console.log("result is"+ revresult)
 
-//       expect(revresult).toBe("anshul")
+// //       expect(revresult).toBe("anshul")
    
-//   });
+// //   });
 
 
-  class Anargra{
+//   class Anargra{
 
-  constructor(page){
-   this.page=page;
-  }
+//   constructor(page){
+//    this.page=page;
+//   }
 
 
-    rearrane(listen , silent){
+//     rearrane(listen , silent){
      
-      const str1=listen.toLowerCase().split('').sort().join();
+//       const str1=listen.toLowerCase().split('').sort().join();
       
-      const str2=silent.toLowerCase().split('').sort().join();
+//       const str2=silent.toLowerCase().split('').sort().join();
 
-      return str1===str2;
-
-
-    }
+//       return str1===str2;
 
 
-  }
-
-test("test the functionality" , async({page})=>{
+//     }
 
 
+//   }
 
-const renameans=new Anargra(page);
-const rensamesaved=renameans.rearrane("listen" , "silent");
-console.log("both wards are anamgram"+rensamesaved);
-expect(rensamesaved).toBe(true);
+// test("test the functionality" , async({page})=>{
 
 
-})
+
+// const renameans=new Anargra(page);
+// const rensamesaved=renameans.rearrane("listen" , "silent");
+// console.log("both wards are anamgram"+rensamesaved);
+// expect(rensamesaved).toBe(true);
 
 
-class palindrome{
- constructor(page){
-   this.page=page;
+// })
 
- }
 
- plaindomeone(recer , madam){
+// class palindrome{
+//  constructor(page){
+//    this.page=page;
 
-   const pone=recer.split('').reverse().join('') && madam.split('').reverse().join('');
-   //const ptwo=madam.split('').reverse().join('');
+//  }
 
-   const word1=recer && madam;
+//  plaindomeone(recer , madam){
+
+//    const pone=recer.split('').reverse().join('') && madam.split('').reverse().join('');
+//    //const ptwo=madam.split('').reverse().join('');
+
+//    const word1=recer && madam;
    
 
 
-   return word1;
+//    return word1;
   
    
 
- }
+//  }
 
-}
+// }
 
-test("test the functionality of plaindrom" , async({page})=>{
+// test("test the functionality of plaindrom" , async({page})=>{
 
-   const plaindomereverse=new palindrome(page);
+//    const plaindomereverse=new palindrome(page);
 
-   const plaindromesave=plaindomereverse.plaindomeone("recer" , "madam");
+//    const plaindromesave=plaindomereverse.plaindomeone("recer" , "madam");
    
-   console.log("wrords are plaindrome" + plaindromesave)
-   expect(plaindromesave).toBeTruthy();
+//    console.log("wrords are plaindrome" + plaindromesave)
+//    expect(plaindromesave).toBeTruthy();
    
       
     
 
-})
+// })
 
-test("test the functionality of login", async({page})=>{
+// test("test the functionality of login", async({page})=>{
 
-const yahoo= new yahoo(page);
-const yahooopen= await page.yahoo("www.yahoo.com");
-await page.goto("www.yahoo.com");
-console.log("yahoo page open successfully" + yahooopen);
-expect(yahooopen).tobeTruthy();
-})
+// const yahoo= new yahoo(page);
+// const yahooopen= await page.yahoo("www.yahoo.com");
+// await page.goto("www.yahoo.com");
+// console.log("yahoo page open successfully" + yahooopen);
+// expect(yahooopen).tobeTruthy();
+// })
 
 
 
--- tests/pratice.spec.js
+++ tests/pratice.spec.js
@@ -1,139 +1,139 @@
-// const {chromium}=require('playwright');
-// const { test , expect } = require('playwright/test');
-// const playwrightConfig = require('./playwright.config');
+// // const {chromium}=require('playwright');
+// // const { test , expect } = require('playwright/test');
+// // const playwrightConfig = require('./playwright.config');
 
  
-//     // test("launch the browser" , async ({})=>{
+// //     // test("launch the browser" , async ({})=>{
 
-//     //     const browser= chromium.launch()
-//     //    const page =(await browser).newPage();
-//     //    await page.goto("https://www.google.com/")
-//     //    console.log(await page.title());
-//     //    await (await browser).close();
+// //     //     const browser= chromium.launch()
+// //     //    const page =(await browser).newPage();
+// //     //    await page.goto("https://www.google.com/")
+// //     //    console.log(await page.title());
+// //     //    await (await browser).close();
     
 
-//     // })
+// //     // })
 
-//     //take a screen Shot
+// //     //take a screen Shot
 
-// //     await page.screenshot({path: 'screenshot.png'})
-// //     await page.screenshot({path : 'fullscreenshot.png' , fullpage: true})
+// // //     await page.screenshot({path: 'screenshot.png'})
+// // //     await page.screenshot({path : 'fullscreenshot.png' , fullpage: true})
 
-// //     this.bowserimage="defined xpath"
-// //     this.iagepath=" image of thepath.png"
+// // //     this.bowserimage="defined xpath"
+// // //     this.iagepath=" image of thepath.png"
 
-// //     async uploadimage()
-// //     if(!this.filepath) throw new error('filepath not found');
-// //     await page.setinputfile(this.bowserimage , this.iagepath);
+// // //     async uploadimage()
+// // //     if(!this.filepath) throw new error('filepath not found');
+// // //     await page.setinputfile(this.bowserimage , this.iagepath);
 
-// //  PWDEBUG=1 npx playwright test for bedbugging..PWDEBUG
+// // //  PWDEBUG=1 npx playwright test for bedbugging..PWDEBUG
 
-// //  await page.pause() for pause the page where operation it is.
+// // //  await page.pause() for pause the page where operation it is.
 
-// module.exports={
-// project :[
+// // module.exports={
+// // project :[
 
-// {name: 'chromium', use :{browsername : 'chromium'}}
+// // {name: 'chromium', use :{browsername : 'chromium'}}
 
-// ]
-// }
-
-
-// module.exports={
-
-//     projects:[
-
-//         {name: 'chromium' , use:{browsername: 'chromium'}}
-//     ]
+// // ]
+// // }
 
 
+// // module.exports={
 
-// }
+// //     projects:[
 
-// // Launch the browser
-// const {chromium} = require('playwright')
-// const{test , expect} = require('@playwright/test')
-//  const browser= chromium.launch();
-//  const page= await page.browser.newpage();
-//  await page.goto("www.google.com");
-//  console.log("google chrome is launch")
+// //         {name: 'chromium' , use:{browsername: 'chromium'}}
+// //     ]
 
-//  //take the screen shot
-//  class screenshot{
-//     constructor (page){;
-//     this.page();
-//     this.browseimage='xpath of browse button';
-//     this.pathofimage='image of path'
-// }
 
-// async browseimage(){
-// await this.page.click(this.browseimage , this.pathofimage)
 
-// }
-//  }
+// // }
 
-//  const image= new screenshot(page);
-//  await image.browseimage();
+// // // Launch the browser
+// // const {chromium} = require('playwright')
+// // const{test , expect} = require('@playwright/test')
+// //  const browser= chromium.launch();
+// //  const page= await page.browser.newpage();
+// //  await page.goto("www.google.com");
+// //  console.log("google chrome is launch")
 
-//  //Screenshot image
-//  await page.screenshot({path: screenshot.png})
-//  //screenshot of full page
-//  await page.screenshot({path: 'fullscreenshot.png' , fullpage: true})
+// //  //take the screen shot
+// //  class screenshot{
+// //     constructor (page){;
+// //     this.page();
+// //     this.browseimage='xpath of browse button';
+// //     this.pathofimage='image of path'
+// // }
 
-//  //inproject config : 
-//  module.exports={
-//   Project:[
+// // async browseimage(){
+// // await this.page.click(this.browseimage , this.pathofimage)
+
+// // }
+// //  }
+
+// //  const image= new screenshot(page);
+// //  await image.browseimage();
+
+// //  //Screenshot image
+// //  await page.screenshot({path: screenshot.png})
+// //  //screenshot of full page
+// //  await page.screenshot({path: 'fullscreenshot.png' , fullpage: true})
+
+// //  //inproject config : 
+// //  module.exports={
+// //   Project:[
     
-//        {name: 'chromium' , use: {browsername: 'chromium'}}
+// //        {name: 'chromium' , use: {browsername: 'chromium'}}
 
-//   ]
-// }
+// //   ]
+// // }
 
 
-// launch the browser chrome
-const{chromium}=require('playwright');
-const{test , expect}=require('@playwright/test');
-const { Interface } = require('readline');
-const browser=new chrome.launch();
-await page.browser.newpage();
-await page.goto("www.google.com");
-console.launch("google chrome is launch");
+// // launch the browser chrome
+// const{chromium}=require('playwright');
+// const{test , expect}=require('@playwright/test');
+// const { Interface } = require('readline');
+// const browser=new chrome.launch();
+// await page.browser.newpage();
+// await page.goto("www.google.com");
+// console.launch("google chrome is launch");
 
-// define the method and call this method in our code
-class callmethod{
-    constructor(page){
-        this.page=page;
-        this.browseimage="xpath of the browser image";
-        this.imagepath="image of the path"
-    }
+// // define the method and call this method in our code
+// class callmethod{
+//     constructor(page){
+//         this.page=page;
+//         this.browseimage="xpath of the browser image";
+//         this.imagepath="image of the path"
+//     }
 
-    async browserimage(){
-        await this.page.click(this.browserimage , this.imagepath)
+//     async browserimage(){
+//         await this.page.click(this.browserimage , this.imagepath)
 
-    }
+//     }
 
-    async screenshot(){
-        await this.page.screenshot({path: 'screenshot.png'})
-        await this.page.screenshot({path:'fullscreenshot.png' , fullpage:true})
+//     async screenshot(){
+//         await this.page.screenshot({path: 'screenshot.png'})
+//         await this.page.screenshot({path:'fullscreenshot.png' , fullpage:true})
     
 
-  // set the viewport size
-     await page.setviewportsize({weight : 1536 , height: 864})  // this set your page to specific resolution
-     // we can define in the project config file
-        module.exports[{
+//   // set the viewport size
+//      await page.setviewportsize({weight : 1536 , height: 864})  // this set your page to specific resolution
+//      // we can define in the project config file
+//         module.exports[{
 
-            project:[
-                {name:'chromium' , use: {browsername:'chromium'}}
-            ]
-        }
+//             project:[
+//                 {name:'chromium' , use: {browsername:'chromium'}}
+//             ]
+//         }
 
 
-        ]
-    }
-}
+//         ]
+//     }
+// }
 
-  // switching Iframe
-  await page.locator("xpath").locator("submitbutton").click();
+//   // switching Iframe
+//   await page.locator("xpath").locator("submitbutton").click();
   
         
 
